6805d1008d880111af13cdfc5f3b3651b710e982
aw18 <wong.albertina@gmail.com>
2017-06-24 14:23:57 -0700

    removed hello

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pysrt
+import nltk
 
 subs = pysrt.open('subtitles/taxi_driver.srt')
 
@@ -6,7 +7,6 @@ len(subs)
 
 first_sub = subs[0]
 
-print "hello"
 print first_sub.text;
 print str(first_sub.start.minutes) + " minutes and " + str(first_sub.start.seconds);
 print str(first_sub.end.minutes) + " minutes and " + str(first_sub.end.seconds);
