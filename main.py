86d575cde82557d30375bad37083cb3946f90da7
nomnombot <hun.nomin@gmail.com>
2017-06-25 12:20:17 -0700

    make top function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import nltk
 import mix
 import json
 import numpy
-
+'''
 nlp = spacy.load('en_core_web_md')
 subs = pysrt.open('subtitles/haine.srt', encoding='iso-8859-1')
 
@@ -29,7 +29,6 @@ def parse_cue(sub):
     minutes = sub.end.minutes * 60000
     seconds = sub.end.seconds * 1000
     cue = (hours + minutes + seconds, "laugh.wav")
-    print("event: {}:{}:{}".format(sub.end.hours,sub.end.minutes,sub.end.seconds))
     return cue
 
 for sub in subs:
@@ -39,9 +38,9 @@ for sub in subs:
     score = target.similarity(nlp(sub.text))
     #print(sub.text, score)
     rawScores.append(score)
-    if score > 0.5: 
-        print(sub.text, score)
-    #    cues.append(parse_cue(sub))
+    #cues.append(parse_cue(sub))
+    #if score > 0.5:
+    #    print(sub.text, score)
 
 #print(rawScores)
 #print(cues)
@@ -57,15 +56,30 @@ mean = numpy.mean(rawScores)
 top_five = []
 _TOP_SCORES = 5
 
-for score in rawScores:
-    if score > std_dev + mean:
-        if len(top_five) < _TOP_SCORES:
-             top_five.append(cues)
-        else:
-            if score > top_five[0]:
-                top_five[0] = score
-        top_five.sort()
+rawScores_cues = zip(rawScores, rawTings)
+sorted_top_pick = sorted(rawScores_cues)[-5:]
 
-print top_five
+
+#print top_five
 
 #mix.add_wavs(cues, video)
+'''
+def get_top_scores_and_timestamps (subtitle_path, target, n):
+    nlp = spacy.load('en_core_web_md')
+    subs = pysrt.open(subtitle_path, encoding='iso-8859-1')
+
+    cues = []
+    raw_scores = []
+
+    target = nlp(u(target))
+
+    for sub in subs:
+        score = target.similarity(nlp(sub.text))
+        raw_scores.append(score)
+        cues.append(parse_cue(sub))
+
+    std_dev = numpy.std(raw_scores)
+    mean = numpy.mean(raw_scores)
+
+    raw_scores_and_cues = zip(raw_scores, cues)
+    return sorted(raw_scores_and_cues)[-5:] 
