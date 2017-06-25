e305f9416b45cd522a31d026c8224d4361d35a30
nomnombot <hun.nomin@gmail.com>
2017-06-25 13:43:40 -0700

    use get_to_scores_and_timestamps function

--- main.py
+++ main.py
@@ -4,33 +4,41 @@ import nltk
 import mix
 import numpy as np
 
-nlp = spacy.load('en_core_web_md')
-subs = pysrt.open('subtitles/casablanca.srt', encoding='iso-8859-1')
+_SUBTITLE_PATH = 'subtitles/taxi_driver.srt'
 
-len(subs)
 
-#first_sub = subs[0]
+def get_top_scores_and_timestamps (subtitle_path, e, n):
+    nlp = spacy.load('en_core_web_md')
+    subs = pysrt.open(subtitle_path, encoding='iso-8859-1')
 
-#print first_sub.text;
-#print str(first_sub.start.minutes) + " minutes and " + str(first_sub.start.seconds);
-#print str(first_sub.end.minutes) + " minutes and " + str(first_sub.end.seconds);
+    cues = []
+    raw_scores = []
 
-#print subs.slice(starts_after={'minutes': 2, 'seconds': 30}, ends_before={'minutes': 2, 'seconds': 40}).text;
+    target = nlp(u(e.phrase))
 
-cues = []
+    for sub in subs:
+        score = e.score(sub.text)
+        raw_scores.append(score)
+        cues.append(parse_cue(sub, e.wav))
+
+    std_dev = np.std(raw_scores)
+    mean = np.mean(raw_scores)
+
+    raw_scores_and_cues = zip(raw_scores, cues)
+    return map(lambda x: x[1], sorted(raw_scores_and_cues)[-5:])
+
+cues = ()
 
 class emotion:
-    def __init__(self, wav, phrase, thresh):
+    def __init__(self, wav, phrase):
         self.wav = wav
         self.phrase = phrase
-        self.vec = nlp(phrase)
-        self.thresh = thresh
 
     def score(self, query):
         return self.vec.similarity(nlp(query))
 
 emotions = [#emotion("laugh.wav", u'happy', 0.72),
-            emotion("awww.mp3", u'that\'s cute', 0.78),]
+            emotion("awww.mp3", u'that\'s cute')]
 emo_scores = {}
 def parse_cue(sub, emo):
     hours = sub.end.hours * 3600000
@@ -40,20 +48,21 @@ def parse_cue(sub, emo):
     print("\t{} event: {}:{}:{}".format(emo, sub.end.hours,sub.end.minutes,sub.end.seconds))
     return cue
 
-for sub in subs:
-    if not "kid" in sub.text and not "friendship" in sub.text:
-        continue
+for emo in emotions:
 
-    n = nlp(sub.text) 
+    '''
+    score = emo.score(sub.text)
+    if emo.wav not in emo_scores:
+        emo_scores[emo.wav] = []
+    emo_scores[emo.wav].append(score)
+    if score > emo.thresh:
+        print(sub.text, score)
+        cues.append(parse_cue(sub, emo.wav))
+    '''
+    top_picks = get_top_scores_and_timestamps(_SUBTITLE_PATH, emo, 5)
+    for top_pick in top_picks:
+        cues.append(top_pick, emo.wav)
 
-    for emo in emotions:
-        score = emo.score(sub.text)
-        if emo.wav not in emo_scores:
-            emo_scores[emo.wav] = []
-        emo_scores[emo.wav].append(score)
-        if score > emo.thresh:
-            print(sub.text, score)
-            cues.append(parse_cue(sub, emo.wav))
 
 #print(cues)
 #ts, wav = cues[0]
